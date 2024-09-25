29ede5ec1bf8b2cea7ad06afb42666b4615d6ed3
J <jpham26@gmail.com>
2024-09-25 13:30:17 -0700

    Add project files

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+http.createServer(function (req, res) {
+  const q = url.parse(req.url, true);
+  const filename = "." + q.pathname;
+  fs.readFile(filename, function (err, data) {
+    if (err) {
+      res.readFile('404.html')
+      return res.end();
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
