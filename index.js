a83856d5934b58918501d5f37bbbee6f5bf36c2b
J <jpham26@gmail.com>
2024-09-25 14:20:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const path = require('path');
 
-http.createServer(function (req, res) {
+http.createServer((req, res) => {
   const q = url.parse(req.url, true);
-  const filename = "." + q.pathname;
-  fs.readFile(filename, function (err, data) {
+  let filename = "." + q.pathname;
+
+  if (filename === './') {
+    filename = './index.html';
+  } else {
+    if (!path.extname(filename)) {
+      filename += '.html';
+    }
+  }
+
+  fs.readFile(filename, (err, data) => {
     if (err) {
-      res.readFile('404.html')
+      fs.readFile('./404.html', (err404, data404) => {
+        if (err404) {
+          res.writeHead(404, { 'Content-Type': 'text/plain' });
+          return res.end('404 Not Found');
+        } else {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          res.write(data404);
+          return res.end();
+        }
+      });
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
       return res.end();
     }
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(data);
-    return res.end();
   });
 }).listen(8080);
